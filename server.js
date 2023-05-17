2946c78fec72ee57cc596a4bffd0943e90d160a8
Samantha Mcmillion <sammy.mcmillion@gmail.com>
2023-05-16 20:24:45 -0700

    server-side proxy

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const PORT = 3000;
+
+const request = require('request');
+
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
@@ -26,6 +29,24 @@ app.get('/', (req, res) => {
 app.post('/weather', (req, res) => {
   const forecastDays = req.body.forecastDays;
   res.render('weather', { forecastDays });
+});
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/forecast', (req, res) => {
+  request(
+    { url: `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&days=3&aqi=no&alerts=no` },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: error.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  )
 });
 
 app.listen(PORT, () => {
